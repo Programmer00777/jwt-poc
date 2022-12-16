fc389d68dd225e1d866167623c41ac90eeeeba78
Programmer00777 <55809302+Programmer00777@users.noreply.github.com>
2022-12-16 13:23:57 +0200

    TokenCipher implemented

--- src/main/java/com/codelib/jwt/constant/Constants.java
+++ src/main/java/com/codelib/jwt/constant/Constants.java
@@ -1,4 +1,11 @@
 package com.codelib.jwt.constant;
 
+import java.io.File;
+
+/**
+ * Project constants
+ */
 public class Constants {
+    public final static String JDBC_URL = "jdbc:h2:file:" + new File("target/store").getAbsolutePath()
+            + "; INIT=RUNSCRIPT FROM 'classpath:db_creation.sql'; DB_CLOSE_ON_EXIT=TRUE;";
 }
--- src/main/java/com/codelib/jwt/crypto/TokenCipher.java
+++ src/main/java/com/codelib/jwt/crypto/TokenCipher.java
@@ -1,4 +1,74 @@
 package com.codelib.jwt.crypto;
 
+import com.google.crypto.tink.Aead;
+import com.google.crypto.tink.KeysetHandle;
+import com.google.crypto.tink.aead.AeadConfig;
+import com.google.crypto.tink.aead.AeadFactory;
+
+import javax.xml.bind.DatatypeConverter;
+
+/**
+ * Handle ciphering and deciphering of the token using AES-GMC.
+ * <p>
+ * Use Google TINK to handle ciphering operations in order to use
+ * crypto built-in best practices provided by this library.
+ */
 public class TokenCipher {
+    /**
+     * Constructor – Register AEAD configuration.
+     *
+     * @throws Exception If any issue occur during AEAD configuration registration
+     */
+    public TokenCipher() throws Exception {
+        AeadConfig.register();
+    }
+
+    /**
+     * Cipher a JWT
+     *
+     * @param jwt Token to cipher
+     * @param keysetHandle Pointer to the keyset handle
+     * @return The ciphered version of the token encoded in HEX
+     * @throws Exception If any issue occur during token ciphering operation
+     */
+    public String cipherToken(String jwt, KeysetHandle keysetHandle) throws Exception {
+        // Verify parameters
+        if (jwt == null || jwt.isEmpty() || keysetHandle == null) {
+            throw new IllegalArgumentException("Both parameters must be provided!");
+        }
+
+        // Get the primitive
+        Aead aead = AeadFactory.getPrimitive(keysetHandle);
+
+        // Cipher the token
+        byte[] cipheredToken = aead.encrypt(jwt.getBytes(), null);
+
+        return DatatypeConverter.printHexBinary(cipheredToken);
+    }
+
+    /**
+     * Decipher a JWT
+     *
+     * @param jwtInHex Token to decipher encoded in HEX
+     * @param keysetHandle Pointer to the keyset handle
+     * @return The token in clear text
+     * @throws Exception If any issue occur during token deciphering operation
+     */
+    public String decipherToken(String jwtInHex, KeysetHandle keysetHandle) throws Exception {
+        // Verify parameters
+        if (jwtInHex == null || jwtInHex.isEmpty() || keysetHandle == null) {
+            throw new IllegalArgumentException("Both parameters must be provided!");
+        }
+
+        // Decode the ciphered token
+        byte[] cipheredToken = DatatypeConverter.parseHexBinary(jwtInHex);
+
+        // Get the primitive
+        Aead aead = AeadFactory.getPrimitive(keysetHandle);
+
+        // Decipher the token
+        byte[] decipheredToken = aead.decrypt(cipheredToken, null);
+
+        return DatatypeConverter.printHexBinary(decipheredToken);
+    }
 }
